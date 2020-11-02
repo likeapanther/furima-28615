aea292b9539ddbfbafcd63dcbdc3710e9e62eb3f
lapan <uec3futsumi@gmail.com>
2020-11-02 12:49:39 +0900

    ユーザー情報のテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
+  VALID_PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #validates :nickname, null: false
-  #validates :email, null: false, unique:true
-  #validates :password, null: false
-  validates :last_name, null: false
-  validates :first_name, null: false
-  validates :last_name_kana, null: false
-  validates :first_name_kana, null: false
-  validates :birthday, null: false
+  validates :nickname, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX} 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,83 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-
     @user = FactoryBot.build(:user)
   end
-  describe "" do
-    
-  end
-  
-  it "test" do
 
+  describe "ユーザー登録" do  
+    it "新規作成できるか" do
+      expect(@user).to be_valid
+    end
+    context '新規登録がうまくいかない時' do
+      it "ニックネームが必須であること" do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "メールアドレスが必須であること" do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+
+      end
+      it "メールアドレスが一意性であること" do
+        @user.save
+        subuser = FactoryBot.build(:user)
+        subuser.email = @user.email
+        subuser.valid?
+        expect(subuser.errors.full_messages).to include("Email has already been taken")
+      end
+      it "メールアドレスは@を含む必要があること" do
+          @user.email = "aaaa.aa"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Email is invalid")
+          
+      end
+      it "パスワードが必須であること" do 
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+
+      end
+      it "パスワードは6文字以上であること" do
+        @user.password = "aaa1"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "パスワードは半角英数字混合であること" do
+        @user.password = "aaaaa"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      
+      end
+      it "パスワードは確認用を含めて2回入力すること" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+      end
+
+    end
+    context 'エラーハンドリング' do
+      it "新規登録時エラーメッセージの出力" do
+        @user.nickname = ""
+        @user.valid?
+
+      end
+
+      it "ログイン時エラーメッセージの出力" do
+        
+      end
+    end
+    context '本人情報確認' do
+      it "ユーザー本名が、名字と名前がそれぞれ必須であること" do 
+      end
+      it "ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること" do end
+      it "ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること" do end
+      it "ユーザー本名のフリガナは全角（カタカナ）で入力させること" do end
+      it "生年月日が必須であること" do end
+    end
   end
 end
