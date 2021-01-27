f94b3ba1ca577d7529fcf3a7ea7e7840d30a776d
lapan <uec3futsumi@gmail.com>
2021-01-27 17:25:55 +0900

    itemのvalidate追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,19 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :price, inclusion: {in: 300..9999999},format: { with: [0-9] }
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :day_id
+  end
+
+
 end
