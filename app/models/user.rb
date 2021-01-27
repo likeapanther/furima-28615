7f2d0bd237ac4c9f03ea15f243ebd71e9d5bf197
lapan <uec3futsumi@gmail.com>
2021-01-27 12:35:58 +0900

    userモデルのvalidates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ }
+      validates :last_name_kana
+      validates :first_name_kana 
+    end
+    validates :birthday
   end
-  validates :birthday, presence: true
 end
