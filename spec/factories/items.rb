f30c1534eaa047406f53a518de8c1119b1f74471
lapan <uec3futsumi@gmail.com>
2021-01-28 22:46:39 +0900

    単体テストコードの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    title { Faker::Book.title }
+    description { Faker::Lorem.sentences }
+    category_id { 2 }
+    condition_id { 2 }
+    fee_id { 2 }
+    prefecture_id { 2 }
+    day_id { 2 }
+    price { Faker::Number.between(from:300, to: 9999999) }
+    association :user
   end
 end
