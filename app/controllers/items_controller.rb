934485f189453b7dd4543f5272d19184fa359da0
lapan <uec3futsumi@gmail.com>
2021-01-30 22:08:24 +0900

    修正、rubocopの適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only:[:edit,:show]
+  before_action :set_item, only: [:edit, :show]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def create
@@ -25,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
   end
 
   private
