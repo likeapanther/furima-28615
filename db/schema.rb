55933f12cb5c54e1fb4b4eb94a559e34907dc64d
lapan <uec3futsumi@gmail.com>
2021-02-01 01:00:52 +0900

    クレジットカード機能の導入
    - Payjpアカウント登録
    - gemのインストール
    - テーブルの作成
    - card.jsの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_053314) do
+ActiveRecord::Schema.define(version: 2021_01_31_155448) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2021_01_31_053314) do
     t.string "number", null: false
     t.string "building"
     t.string "tel", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "customer_id"
+    t.string "card_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
