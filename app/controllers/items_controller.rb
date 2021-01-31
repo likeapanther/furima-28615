d5fe86a0c930bfb4e024bb005523fafce1e87a72
lapan <uec3futsumi@gmail.com>
2021-01-31 12:41:46 +0900

    controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit,:update,:destroy]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -25,9 +25,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user_id 
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   def update
