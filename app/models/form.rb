9ed38b72515fe73dcf34c336ab8ec7af97c19c11
lapan <uec3futsumi@gmail.com>
2021-02-01 22:28:30 +0900

    バリデーションの設定

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,19 +2,15 @@ class Form
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :price, :token, :purchase_id, :zip, :prefecture_id, :city, :number, :building, :tel
   
-  #with_options presence: true do
-    #validates :zip, format: { with: /\A\d{3}[-]\d{4}\z/}
-    #validates :prefecture_id
-    #validates :city
-    #validates :number
-    #validates :tel,format: { with: /^\d{11}$/}
-    #validates :price
-    #validates :token
-  #end
+  with_options presence: true do
+    validates :zip, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: {only_integer: true}
+    validates :city
+    validates :number
+    validates :tel,format: { with: /\A\d{11}\z/}
+    validates :token
+  end
 
-  #with_options numericality: {only_integer: true} do
-  #  validates :prefecture_id
-  #end
 
   def save
     @purchase = Purchase.create(user_id: user_id,item_id: item_id)
