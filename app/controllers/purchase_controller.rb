8f18390390bf186b94bf7d14dad1cdfe002532af
lapan <uec3futsumi@gmail.com>
2021-02-02 11:32:13 +0900

    purchaseにデータが有るかのチェックのメソッド化

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,28 +1,21 @@
 class PurchaseController < ApplicationController
   before_action :authenticate_user!
-  # before_action :item_params, only: [:index,:create]
+  before_action :set_item, only: [:index,:create]
+  before_action :check_item, only: [:index,:create]
 
   def index
-    # 購入情報に商品がない場合
-    if Purchase.find_by(item_id: params[:item_id]).nil?
       # 閲覧しているユーザーと出品者が違う場合
       if current_user.id != Item.find_by_id(params[:item_id]).user_id
         @form = Form.new
         @user = current_user
-        @item = Item.find(params[:item_id])
 
       else
         redirect_to root_path
       end
-    else
-      redirect_to root_path
-    end
+
   end
 
   def create
-    # すでに購入されているとき
-    if Purchase.find_by(item_id: params[:item_id]).nil?
-      @item = Item.find(params[:item_id])
       @form = Form.new(form_params)
       if @form.valid?
         pay_item
@@ -31,19 +24,25 @@ class PurchaseController < ApplicationController
       else
         render :index
       end
-    else
-      redirect_to root_path
-    end
   end
 
   private
+
+  def check_item
+    #すでに購入されているとき
+    if Purchase.find_by(item_id: params[:item_id]).nil?
+    else
+      redirect_to root_path
+    end
+
+  end
 
   def form_params
     params.require(:form).permit(:zip, :prefecture_id, :city, :number, :building, :tel).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
