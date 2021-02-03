73f3297bf9335256c8bbb943b9e51266af1650e6
lapan <uec3futsumi@gmail.com>
2021-02-03 10:46:58 +0900

    Update forms.rb

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     zip           { "123-4567" }
     prefecture_id { 5 }
     city          { '横浜市' }
-    number        { 5 }
+    number        { "５丁目３−１" }
     building      { 'マンション' }
     tel           { 11122223333 }
     token         { 'aaa' }
